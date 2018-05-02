c8f5fd90589c6e9122f3bbe446f0d6908419aea6
Morgan Fudge <mln_mf@hotmail.com>
2018-05-02 13:04:05 -0230

    Added logout link Updated routing

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,6 @@
 class ApplicationController < ActionController::Base
     def after_sign_in_path_for(resource)
-        home_index_url
+        home_url
     end
     def after_sign_out_path_for(resource_or_scope)
         root_path
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  get 'home/index'
+  get 'home', to: 'home#index'
   devise_for :users, controllers: { omniauth_callbacks: 'users/omniauth_callbacks' }
   devise_scope :user do
     root to: "devise/sessions#new"
