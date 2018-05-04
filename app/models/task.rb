8f0b9f369e0af4138197b9125b88493ebc9096ac
Morgan Fudge <mln_mf@hotmail.com>
2018-05-04 06:31:01 -0230

    Added task validations

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,4 +1,7 @@
 class Task < ApplicationRecord
+    validates :title, presence: true
+    validates :tag_id, presence: true
+
     belongs_to :project
     belongs_to :tag
     has_and_belongs_to_many :users
