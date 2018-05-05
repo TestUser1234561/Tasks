dec3303544c2db37b26b0477ff48d41d7d8f9360
Morgan Fudge <mln_mf@hotmail.com>
2018-05-05 05:55:04 -0230

    Added overflow styles

--- app/assets/javascripts/main.js
+++ app/assets/javascripts/main.js
@@ -12,4 +12,7 @@ $(document).on('turbolinks:load', function() {
             })
         }
     })
+
+    //Hack min width to nav bar -- lazy
+    document.body.style.minWidth = `${$('#title').width() + $('#button-dashboard').width() + 40}px`
 });
