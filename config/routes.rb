c1fbc8afc75590672e754adbf8976beba79d101c
Morgan Fudge <mln_mf@hotmail.com>
2018-05-06 16:23:28 -0230

    Updated routes

--- config/routes.rb
+++ config/routes.rb
@@ -3,14 +3,11 @@ Rails.application.routes.draw do
     get 'dashboard', to: 'home#index'
 
     #Projects
-    get 'project/:project_id/delete', to: 'project#confirm_destroy', as: 'delete_project'
-    get 'project/:project_id/add', to: 'project#add_user', as: 'add_user'
     post 'project/:project_id/add', to: 'project#add'
-    resources :project, except: [:index,], controller: :project do
+    resources :project, except: [:index, :edit], controller: :project do
 
         #tasks
-        get 'task/:task_id/delete', to: 'task#confirm_destroy', as: 'delete_task'
-        resources :task, except: [:index], controller: :task, param: :task_id
+        resources :task, except: [:new, :edit], controller: :task, param: :task_id
 
     end
 
